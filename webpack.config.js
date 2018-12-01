30740e713d2953bc573379cd7e4c49bd90a1b091
Никита Гревцов <n.a.grevtsov@gmail.com>
2018-12-01 13:58:47 +0700

    Добавлен eslint

--- webpack.config.js
+++ webpack.config.js
@@ -4,52 +4,52 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: ['./src/core/main.js', './src/core/main.scss'],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ['@babel/preset-env'],
-                        plugins: ['@babel/plugin-proposal-class-properties']
-                    }
-                }
-            },
-            {
-                test: /\.s?css$/,
-                use: [
-                    'css-hot-loader',
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'resolve-url-loader',
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true,
-                            sourceMapContents: false
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(png|jpg|gif)$/,
-                use: ['file-loader']
+  entry: ['./src/core/main.js', './src/core/main.scss'],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          'css-hot-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'resolve-url-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              sourceMapContents: false
             }
+          }
         ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({filename: 'bundle.css'}),
-        new HtmlWebpackPlugin({
-            template: './src/core/index.html',
-            hash: true,
-        }),
-        new CleanWebpackPlugin('dist')
-    ],
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js'
-    }
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: ['file-loader']
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({filename: 'bundle.css'}),
+    new HtmlWebpackPlugin({
+      template: './src/core/index.html',
+      hash: true,
+    }),
+    new CleanWebpackPlugin('dist')
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  }
 };

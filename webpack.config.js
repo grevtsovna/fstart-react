0540a071fbafdf7ce874cfa152df4d99428a489a
Никита Гревцов <n.a.grevtsov@gmail.com>
2018-12-01 11:42:13 +0700

    В конфиг вебпака добавлен babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,20 @@ const path = require('path');
 
 module.exports = {
     entry: ['./src/core/main.js'],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'bundle.js'

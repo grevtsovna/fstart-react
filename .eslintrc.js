3c6623b3f55ecf4ad75a16739a875e6e4a87e5ed
Никита Гревцов <n.a.grevtsov@gmail.com>
2018-12-09 23:02:29 +0700

    Поправлен конфиг eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,14 @@
 module.exports = {
   "extends": "airbnb",
   "rules": {
-    "linebreak-style": ["error", "windows"]
+    "linebreak-style": ["error", "unix"],
+    "comma-dangle": ["error", "never"],
+  },
+  "settings": {
+    "import/resolver": {
+      "node": {
+        "paths": ["src"]
+      }
+    }
   }
 };

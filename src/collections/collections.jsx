c160219305bad3cbc092fe86a58357a9645fdfa0
Никита Гревцов <n.a.grevtsov@gmail.com>
2018-12-12 17:31:59 +0700

    Добавлены стили

--- src/collections/collections.jsx
+++ src/collections/collections.jsx
@@ -1,12 +1,31 @@
 import React, { PureComponent } from 'react';
 import Collection from 'collection/collection';
+import PropTypes from 'prop-types';
 import axios from 'axios';
+import { withStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary
+  }
+});
 
 class Collections extends PureComponent {
   state = {
     isLoading: true,
     collections: []
+  };
+
+  static propTypes = {
+    classes: PropTypes.object.isRequired
   };
 
   componentDidMount() {
@@ -19,19 +38,28 @@ class Collections extends PureComponent {
   }
 
   render() {
+    const { classes } = this.props;
     const { collections, isLoading } = this.state;
     return (
       <div className="collections">
-        { isLoading && <CircularProgress />}
-        {collections.map(collection => (
-          <Collection
-            key={collection.id}
-            collection={collection}
-          />
-        ))}
+        <div className={classes.root}>
+          <Grid container spacing={24}>
+            { isLoading && <CircularProgress className="collections__preloader" />}
+            {collections.map(collection => (
+              <Grid item xs={12}>
+                <Paper className={classes.paper}>
+                  <Collection
+                    key={collection.id}
+                    collection={collection}
+                  />
+                </Paper>
+              </Grid>
+            ))}
+          </Grid>
+        </div>
       </div>
     );
   }
 }
 
-export default Collections;
+export default withStyles(styles)(Collections);

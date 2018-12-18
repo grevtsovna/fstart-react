668bb5c09edfadd641d72793641806dba3a67308
Никита Гревцов <n.a.grevtsov@gmail.com>
2018-12-18 20:12:11 +0700

    Реализовано удаление словарей

--- src/collection/collection.jsx
+++ src/collection/collection.jsx
@@ -2,47 +2,107 @@ import React, { PureComponent } from 'react';
 import TextField from '@material-ui/core/TextField';
 import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
+import Grid from '@material-ui/core/Grid';
+import { Link } from 'react-router-dom';
+import Paper from '@material-ui/core/Paper';
+import Fab from '@material-ui/core/Fab';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from '@material-ui/core/Button';
+import axios from 'axios';
 
-const styles = theme => ({
+const styles = () => ({
   container: {
     display: 'flex',
     flexWrap: 'wrap'
   },
   textField: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit,
-    width: 200
+    width: '50%'
   },
-  dense: {
-    marginTop: 19
+  paper: {
+    padding: '15px 30px',
+    position: 'relative'
   },
-  menu: {
-    width: 200
+  delete: {
+    position: 'absolute',
+    right: 20,
+    top: '50%',
+    transform: 'translateY(-50%)'
   }
 });
 
 class Collection extends PureComponent {
   state = {
-
+    isAlertOpen: false
   };
 
   static propTypes = {
     collection: PropTypes.object.isRequired,
     classes: PropTypes.object.isRequired,
-    changeName: PropTypes.func.isRequired
+    changeName: PropTypes.func.isRequired,
+    removeCollection: PropTypes.func.isRequired
+  };
+
+  handleClose = () => {
+    console.log('close!');
+    this.setState({ isAlertOpen: false });
+  };
+
+  clickHandle = (evt) => {
+    evt.preventDefault();
+    this.setState({ isAlertOpen: true });
+  };
+
+  removeHandle = () => {
+    const { removeCollection } = this.props;
+    removeCollection();
+    this.handleClose();
   };
 
   render() {
     const { collection, classes, changeName } = this.props;
+    const { isAlertOpen } = this.state;
     return (
-      <TextField
-        id={collection.id}
-        label="Название словаря"
-        className={classes.textField}
-        value={collection.name}
-        onBlur={changeName}
-        onClick={(evt) => { evt.preventDefault(); }}
-      />
+      <Grid item xs={12}>
+        <Link to={`/collections/${collection.id}`}>
+          <Paper className={classes.paper}>
+            <TextField
+              id={collection.id}
+              label="Название словаря"
+              className={classes.textField}
+              value={collection.name}
+              onBlur={changeName}
+              onClick={(evt) => { evt.preventDefault(); }}
+            />
+            <Fab size="small" color="secondary" className={classes.delete} onClick={this.clickHandle}>
+              <DeleteIcon />
+            </Fab>
+          </Paper>
+        </Link>
+        <Dialog
+          open={isAlertOpen}
+          onClose={this.handleClose}
+        >
+          <DialogTitle>Вы действительно хотите удалить словарь &laquo;{collection.name}&raquo;?</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              При удалении словаря, все слова, входящие в него, будут утеряны.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary" autoFocus>
+              Отмена
+            </Button>
+            <Button onClick={this.removeHandle} color="secondary" variant="contained">
+              Удалить
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </Grid>
     );
   }
 }
--- src/collections/collections.jsx
+++ src/collections/collections.jsx
@@ -4,19 +4,12 @@ import PropTypes from 'prop-types';
 import axios from 'axios';
 import { withStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import { Link } from 'react-router-dom';
 import AddCollection from 'add-collection/add-collection';
 
-const styles = theme => ({
+const styles = () => ({
   root: {
     flexGrow: 1
-  },
-  paper: {
-    padding: theme.spacing.unit * 2,
-    textAlign: 'center',
-    color: theme.palette.text.secondary
   }
 });
 
@@ -24,7 +17,7 @@ class Collections extends PureComponent {
   state = {
     isLoading: true,
     collections: [],
-    isAddingCollection: false
+    isAddingCollection: false,
   };
 
   static propTypes = {
@@ -55,6 +48,16 @@ class Collections extends PureComponent {
       });
   };
 
+  removeCollection = (id) => {
+    axios.delete(`/api/v1/collections/${id}`)
+      .then((response) => {
+        console.log(response);
+        this.setState(prevState => ({
+          collections: prevState.collections.filter(collection => response.data.data.id !== collection.id)
+        }));
+      });
+  };
+
   render() {
     const { classes } = this.props;
     const { collections, isLoading, isAddingCollection } = this.state;
@@ -64,16 +67,12 @@ class Collections extends PureComponent {
           <Grid container spacing={24}>
             { isLoading && <CircularProgress className="collections__preloader" />}
             {collections.map(collection => (
-              <Grid item xs={12} key={collection.id}>
-                <Link to={`/collections/${collection.id}`}>
-                  <Paper className={classes.paper}>
-                    <Collection
-                      collection={collection}
-                      changeName={this.changeName}
-                    />
-                  </Paper>
-                </Link>
-              </Grid>
+              <Collection
+                key={collection.id}
+                collection={collection}
+                changeName={this.changeName}
+                removeCollection={this.removeCollection}
+              />
             ))}
             <Grid item xs={12}>
               { !isLoading && (
